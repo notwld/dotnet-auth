70c6d66af03d6af3329c94b0d75c377ec50d9a35
notwld <mwfarrukh@gmail.com>
2023-01-17 01:05:37 +0500

    models

---
+++ dotnet-auth/Models/Constants.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_auth.Models
+{
+    public class Constants
+    {
+        public static List<User> users = new List<User>()
+        {
+            new User
+            {
+                Id=1,
+                Username="Waleed",
+                Password="Salad",
+                Email="mw@gmail.com",
+                Role="CEO"
+            },
+             new User
+            {
+                Id=2,
+                Username="Bajwa",
+                Password="Salad",
+                Email="mw@gmail.com",
+                Role="Project Manager"
+            },
+              new User
+            {
+                Id=3,
+                Username="Farhan",
+                Password="Salad",
+                Email="farhan@gmail.com",
+                Role="Business Analyst"
+            }
+        };
+
+    }
+}
---
+++ dotnet-auth/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_auth.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+    }
+}
